33a99cd5d1eb15ce391e5f5ac08ed97af14cacca
najibdj27 <64320942+najibdj27@users.noreply.github.com>
2022-07-04 09:58:28 +0700

    Create DML_reservations.sql

---
+++ DML_reservations.sql
@@ -0,0 +1,43 @@
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(4, 3, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(2, 1, false, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(1, 2, false, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(6, 1, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(4, 9, false, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(3, 8, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(2, 1, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(5, 1, false, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(1, 6, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(3, 3, true, now(), now());
+
+INSERT INTO public.reservations
+(user_id, schedule_id, is_active, created_at, updated_at)
+VALUES(2, 3, false, now(), now());
